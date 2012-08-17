6156c33af6910d64669ee4550018911516310175
Jason Rush <jason@fizzawizza.com>
2012-08-17 11:52:25 -0500

    Initial Commit

---
+++ Meow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Meow
+//
+//  Created by Jason Rush on 8/17/12.
+//  Copyright (c) 2012 Fizzawizza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Meow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Meow
+//
+//  Created by Jason Rush on 8/17/12.
+//  Copyright (c) 2012 Fizzawizza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
